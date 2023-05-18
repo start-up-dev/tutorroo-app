2e10bb7ea13b341fd1b10b612190d0aa09d202a7
Shanta Miah <coder.shanta@gmail.com>
2023-05-18 06:56:41 +0600

    base url changed

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://tutorroo-server.vercel.app/api";
-export const socketBaseURL = "https://tutorroo-server.vercel.app";
+export const baseURL = "https://tutorroo-api.cyclenation.io/api";
+export const socketBaseURL = "https://tutorroo-api.cyclenation.io";
 // export const baseURL = "http://192.168.1.101:5000/api";
 // export const socketBaseURL = "http://192.168.1.101:5000";
